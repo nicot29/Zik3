3edece5ba0ed659759448c79aa9547f829d84ae1
Nicolas Thomas <nthomas@appnexus.com>
2018-05-03 17:52:51 +0100

    Initial Commit

---
+++ Zik3/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Zik3
+//
+//  Created by Nicolas Thomas on 03/05/2018.
+//  Copyright © 2018 Nicolas Thomas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
